87628886c873bb228925b5fd549b831d6afc6a73
Guddu Kumar <guddu748081@gmail.com>
2025-04-21 00:15:26 +0530

    to do app

---
+++ to-do-app/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/Header'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    </>
+  )
+}
+
+export default App
---
+++ to-do-app/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='header'>
+        Record Keeping
+   </div>
+  )
+}
+
+export default Header
